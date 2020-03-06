755e12fc3974cdfa4ba2dd09f0049843bd58af8c
120512900 <51316715+120512900@users.noreply.github.com>
2020-03-06 14:33:32 +0800

    1

--- app/src/main/java/mg/studio/android/lab001/MainActivity.java
+++ app/src/main/java/mg/studio/android/lab001/MainActivity.java
@@ -2,7 +2,11 @@ package mg.studio.android.lab001;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,10 +14,18 @@ public class MainActivity extends AppCompatActivity {
     ImageView mouth, arms, ears, eyebrows, eyes, glasses, hat, mustache, nose, shoes;
 
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+          setContentView(R.layout.activity_main);
+       setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+
+
         mouth = findViewById(R.id.mouth);
         arms = findViewById(R.id.arms);
         ears = findViewById(R.id.ears);
@@ -26,4 +38,51 @@ public class MainActivity extends AppCompatActivity {
         shoes = findViewById(R.id.shoes);
     }
 
+    private void check(View view ,ImageView image){
+        if(image.getVisibility()==View.GONE){
+            image.setVisibility(View.VISIBLE);
+
+        }else{
+            image.setVisibility(View.GONE);
+        }
+    }
+    public void cbMouth(View view) {
+        check(view,mouth);
+    }
+
+    public void cbArms(View view) {
+        check(view,arms);
+    }
+
+    public void cbEars(View view) {
+        check(view,ears);
+    }
+
+    public void cbHat(View view) {
+        check(view,hat);
+    }
+
+    public void cbEyebrow(View view) {
+        check(view,eyebrows);
+    }
+
+    public void cbEye(View view) {
+        check(view,eyes);
+    }
+
+    public void cbGlasses(View view) {
+        check(view,glasses);
+    }
+
+    public void cbMustache(View view) {
+        check(view,mustache);
+    }
+
+    public void cbNose(View view) {
+        check(view,nose);
+    }
+
+    public void cbShoes(View view) {
+        check(view,shoes);
+    }
 }
